0b9ca54a18c932e0144601b09e9650ef87a89342
surendra reddy <surendrareddylamuru01@gmail.com>
2023-04-18 10:49:56 +0530

    rds

--- alb.tf
+++ alb.tf
@@ -37,6 +37,7 @@ resource "aws_security_group" "main" {
 }
 
 resource "aws_lb_listener" "main" {
+  count             = var.internal ? 1 : 0
   load_balancer_arn = aws_lb.main.arn
   port              = "80"
   protocol          = "HTTP"
@@ -48,6 +49,23 @@ resource "aws_lb_listener" "main" {
       content_type = "text/plain"
       message_body = "Fixed response content"
       status_code  = "200"
+    }
+  }
+}
+
+resource "aws_lb_listener" "public-http" {
+  count             = var.internal ? 0 : 1
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
     }
   }
 }
