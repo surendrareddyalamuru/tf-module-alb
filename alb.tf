624e88e14f62e0e1871b58eb777df7ab675343f3
surendra reddy <surendrareddylamuru01@gmail.com>
2023-04-18 10:54:38 +0530

    rds

--- alb.tf
+++ alb.tf
@@ -23,6 +23,14 @@ resource "aws_security_group" "main" {
     cidr_blocks = var.internal ? [var.vpc_cidr] : ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = var.internal ? [var.vpc_cidr] : ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
